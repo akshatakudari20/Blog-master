b2b7a65b64d66aa50b3af92bb1aaf975feda4824
AKSHATA <akshatakudari20@gmail.com>
2022-02-21 09:59:06 +0530

    my project

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# Blog Website
+
+A simple website to Create, Read, Update and Delete blogs.
+
+### Frontend 
+EJS , Bootstrap
+### Backend 
+Express JS
+### Database 
+Mongo DB
+
+## Sample Pictures
+
+### All Articles
+![allArticles](https://user-images.githubusercontent.com/64946091/116089716-bf660d00-a6c0-11eb-9f18-26622efba783.jpg)
+
+### Edit Article
+![editArticle](https://user-images.githubusercontent.com/64946091/116089839-ddcc0880-a6c0-11eb-855d-5585cf1e88ca.jpg)
+
+### Show Article
+![article](https://user-images.githubusercontent.com/64946091/116089899-ee7c7e80-a6c0-11eb-9192-89f3eb58b774.jpg)
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const articleRouter = require('./routes/articles');
+const Article = require('./models/article');
+
+mongoose.connect('mongodb://localhost/blog', { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+ });
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({
+        createdAt: 'desc'
+    });
+    res.render('articles/index', { articles: articles });
+});
+
+app.use('/articles', articleRouter);
+
+app.listen(5000);
